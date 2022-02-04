0ad68f119fc6d9c2a8e7a1ac59c8994446baf9df
Jerry M <developer.jerrymannel@gmail.com>
2022-02-04 21:05:00 +0530

    Flow schema validation

--- generator/schema.validator.js
+++ generator/schema.validator.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const Ajv = require('ajv');
 const ajv = new Ajv();
 
-let schema = fs.readFileSync('flow.schema.json').toString();
+let schema = fs.readFileSync('../flow.schema.json').toString();
 schema = JSON.parse(schema);
 
-let flowData = fs.readFileSync('./test/test.01.simpleAPI.json').toString();
+let flowData = fs.readFileSync('../test/test.01.simpleAPI.json').toString();
 flowData = JSON.parse(flowData);
 
 const validate = ajv.compile(schema);
 const valid = validate(flowData);
-if (!valid) console.log(validate.errors);
+if (!valid) {
+	console.log(ajv.errorsText(validate.errors));
+}
+else console.log('Valid');
