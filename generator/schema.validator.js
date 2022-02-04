094848ac8bf3f7baa1026f1fc9729d463dce4e88
Jerry M <developer.jerrymannel@gmail.com>
2022-02-04 17:00:57 +0530

    building a schema validator

---
+++ generator/schema.validator.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
+let schema = fs.readFileSync('flow.schema.json').toString();
+schema = JSON.parse(schema);
+
+let flowData = fs.readFileSync('./test/test.01.simpleAPI.json').toString();
+flowData = JSON.parse(flowData);
+
+const validate = ajv.compile(schema);
+const valid = validate(flowData);
+if (!valid) console.log(validate.errors);
