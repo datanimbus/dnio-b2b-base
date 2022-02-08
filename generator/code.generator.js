a62cb747c07f1294f6460cd99151c7053a8f37ca
Jugnu Agrawal <jugnu@appveen.com>
2022-02-08 16:52:32 +0530

    error state

--- generator/code.generator.js
+++ generator/code.generator.js
@@ -47,8 +47,8 @@ function generateCode(dataJson) {
 		code.push(`${tab(1)}    if( tempResponse.statusCode >= 400 ) {`);
 		code.push(`${tab(1)}      state.status = "ERROR";`);
 		code.push(`${tab(1)}      await stateUtils.upsertState(req, state);`);
-		code.push(`${tab(1)}      state = stateUtils.getState(tempResponse, '${item.onError._id}');`);
 		if (item.onError && item.onError.length > 0) {
+			code.push(`${tab(1)}    state = stateUtils.getState(tempResponse, '${item.onError._id}');`);
 			code.push(`${tab(1)}    tempResponse = await stageUtils.${_.camelCase(item.onError._id)}(req, state);`);
 		} else {
 			code.push(`${tab(1)}      return res.status(tempResponse.statusCode).json(tempResponse.body)`);
