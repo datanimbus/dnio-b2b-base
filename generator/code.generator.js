961336d625aa967f5827feb2153674c834cf5207
Jugnu Agrawal <jugnu@appveen.com>
2022-02-14 17:25:45 +0530

    generate uising onSuccess

--- generator/code.generator.js
+++ generator/code.generator.js
@@ -3,6 +3,8 @@ const _ = require('lodash');
 
 // const logger = log4js.getLogger(global.loggerName);
 
+const visitedStages = [];
+
 function tab(len) {
 	let d = '';
 	while (len > 0) {
@@ -16,122 +18,168 @@ function tab(len) {
  * 
  * @param {any} dataJson 
  */
-function generateCode(dataJson) {
+function parseFlow(dataJson) {
 	const inputStage = dataJson.inputStage;
 	const stages = dataJson.stages;
 	let api = inputStage.incoming.path;
 	let code = [];
-	code.push('const router = require(\'express\').Router();');
-	code.push('const log4js = require(\'log4js\');');
-	code.push('');
-	code.push('const stateUtils = require(\'./state.utils\');');
-	code.push('const stageUtils = require(\'./stage.utils\');');
-	code.push('');
-	code.push('const logger = log4js.getLogger(global.loggerName);');
-	code.push('');
+	code.push(`const router = require(\'express\').Router();`);
+	code.push(`const log4js = require(\'log4js\');`);
+	code.push(``);
+	code.push(`const stateUtils = require(\'./state.utils\');`);
+	code.push(`const stageUtils = require(\'./stage.utils\');`);
+	code.push(``);
+	code.push(`const logger = log4js.getLogger(global.loggerName);`);
+	code.push(``);
 	// TODO: Method to be fixed.
 	code.push(`router.post('${api}', async function (req, res) {`);
 	code.push(`${tab(1)}let txnId = req.headers['data-stack-txn-id'];`);
 	code.push(`${tab(1)}let remoteTxnId = req.headers['data-stack-remote-txn-id'];`);
 	code.push(`${tab(1)}let state = {};`);
 	code.push(`${tab(1)}let tempResponse = req;`);
-	stages.forEach((item, i) => {
-		const isLast = stages.length - 1 == i;
-		code.push(`${tab(1)}// ═══════════════════ ${item._id} / ${item.name} / ${item.type} ══════════════════════`);
-		code.push(`${tab(1)}logger.debug(\`[\${txnId}] [\${remoteTxnId}] Invoking stage :: ${item._id} / ${item.name} / ${item.type}\`)`);
-		code.push(`${tab(1)}state = stateUtils.getState(tempResponse, '${item._id}');`);
-		code.push(`${tab(1)}try {`);
-		code.push(`${tab(1)}    tempResponse = await stageUtils.${_.camelCase(item._id)}(req, state);`);
-		code.push(`${tab(1)}    state.statusCode = tempResponse.statusCode;`);
-		code.push(`${tab(1)}    state.body = tempResponse.body;`);
-		code.push(`${tab(1)}    if( tempResponse.statusCode >= 400 ) {`);
-		code.push(`${tab(1)}      state.status = "ERROR";`);
-		code.push(`${tab(1)}      await stateUtils.upsertState(req, state);`);
-		if (item.onError && item.onError.length > 0) {
-			code.push(`${tab(1)}    state = stateUtils.getState(tempResponse, '${item.onError._id}');`);
-			code.push(`${tab(1)}    tempResponse = await stageUtils.${_.camelCase(item.onError._id)}(req, state);`);
-		} else {
-			code.push(`${tab(1)}      return res.status(tempResponse.statusCode).json(tempResponse.body)`);
+	inputStage.onSuccess.map(ss => stages.find(e => e._id === ss._id)).forEach((stage, i) => {
+		if (visitedStages.indexOf(stage._id) > -1) {
+			return;
 		}
-		code.push(`${tab(1)}    }`);
-		code.push(`${tab(1)}    state.status = "SUCCESS";`);
-		code.push(`${tab(1)}    await stateUtils.upsertState(req, state);`);
-		if (isLast) {
-			code.push(`${tab(1)}    res.status(tempResponse.statusCode).json(tempResponse.body)`);
-		}
-		code.push(`${tab(1)}} catch (err) {`);
-		code.push(`${tab(1)}    logger.error(err);`);
-		code.push(`${tab(1)}    return res.status(500).json({ message: err.message });`);
-		code.push(`${tab(1)}}`);
+		visitedStages.push(stage._id);
+		code = code.concat(generateCode(stage, stages));
 	});
-	code.push('});');
-	code.push('module.exports = router;');
+	code.push(`});`);
+	code.push(`module.exports = router;`);
 	return code.join('\n');
 }
 
+/**
+ * 
+ * @param {any} dataJson 
+ */
+function generateCode(stage, stages) {
+	let code = [];
+	code.push(`${tab(1)}// ═══════════════════ ${stage._id} / ${stage.name} / ${stage.type} ══════════════════════`);
+	code.push(`${tab(1)}logger.debug(\`[\${txnId}] [\${remoteTxnId}] Invoking stage :: ${stage._id} / ${stage.name} / ${stage.type}\`)`);
+	code.push(`${tab(1)}state = stateUtils.getState(tempResponse, '${stage._id}');`);
+	code.push(`${tab(1)}try {`);
+	code.push(`${tab(2)}tempResponse = await stageUtils.${_.camelCase(stage._id)}(req, state);`);
+	code.push(`${tab(2)}state.statusCode = tempResponse.statusCode;`);
+	code.push(`${tab(2)}state.body = tempResponse.body;`);
+	code.push(`${tab(2)}if (tempResponse.statusCode >= 400) {`);
+	code.push(`${tab(3)}state.status = "ERROR";`);
+	code.push(`${tab(3)}await stateUtils.upsertState(req, state);`);
+	if (stage.onError && stage.onError.length > 0) {
+		code.push(`${tab(3)}state = stateUtils.getState(tempResponse, '${stage.onError._id}');`);
+		code.push(`${tab(3)}tempResponse = await stageUtils.${_.camelCase(stage.onError._id)}(req, state);`);
+	} else {
+		code.push(`${tab(3)}return res.status(tempResponse.statusCode).json(tempResponse.body)`);
+	}
+	code.push(`${tab(2)}}`);
+	code.push(`${tab(2)}state.status = "SUCCESS";`);
+	code.push(`${tab(2)}await stateUtils.upsertState(req, state);`);
+	code.push(`${tab(1)}} catch (err) {`);
+	code.push(`${tab(2)}logger.error(err);`);
+	code.push(`${tab(2)}return res.status(500).json({ message: err.message });`);
+	code.push(`${tab(1)}}`);
+	stage.onSuccess.map(ss => stages.find(e => e._id === ss._id)).forEach((stage, i) => {
+		if (visitedStages.indexOf(stage._id) > -1) {
+			return;
+		}
+		visitedStages.push(stage._id);
+		code = code.concat(generateCode(stage, stages));
+	});
+	return code.join('\n');
+}
 
-function generateStages(dataJson) {
-	const stages = dataJson.stages;
+function parseStages(dataJson) {
 	const code = [];
+	code.push(`const log4js = require(\'log4js\');`);
+	code.push(`const _ = require(\'lodash\');`);
+	code.push(`const httpClient = require(\'./http-client\');`);
+	code.push(``);
+	code.push(`const logger = log4js.getLogger(global.loggerName);`);
+	code.push(``);
+	return _.concat(code, generateStages(dataJson)).join('\n');
+}
+
+
+function generateStages(stage) {
+	const stages = stage.stages;
+	let code = [];
 	const exportsCode = [];
-	code.push('const log4js = require(\'log4js\');');
-	code.push('const _ = require(\'lodash\');');
-	code.push('const httpClient = require(\'./http-client\');');
-	code.push('');
-	code.push('const logger = log4js.getLogger(global.loggerName);');
-	code.push('');
 	stages.forEach((stage) => {
 		exportsCode.push(`module.exports.${_.camelCase(stage._id)} = ${_.camelCase(stage._id)};`);
 		code.push(`async function ${_.camelCase(stage._id)}(req, state) {`);
-		code.push(`logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Starting ${_.camelCase(stage._id)} Stage\`);`);
-		code.push('    try {');
-		if ((stage.type === 'API' || stage.type === 'FAAS') && stage.outgoing) {
-			code.push('const options = {};');
-			code.push(`state.url = '${stage.outgoing.url}';`);
-			code.push(`state.method = '${stage.outgoing.method}';`);
-			code.push(`options.url = '${stage.outgoing.url}';`);
-			code.push(`options.method = '${stage.outgoing.method}';`);
-			code.push(`options.headers = _.merge(state.headers,${JSON.stringify(stage.outgoing.headers)});`);
-			code.push('options.json = state.body;');
-			code.push('try {');
-			code.push('  const tempResponse = await httpClient.request(options);');
-			code.push('  if( tempResponse && tempResponse.statusCode != 200 ) {');
-			code.push(`    logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with not 200\`);`);
-			code.push('    return { statusCode: tempResponse.statusCode, body: tempResponse.body, headers: tempResponse.headers };');
-			code.push('  }');
-			code.push(`  logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with 200\`);`);
-			code.push('  return { statusCode: tempResponse.statusCode, body: tempResponse.body, headers: tempResponse.headers };');
-			code.push('} catch(err) {');
-			code.push(`  logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with Error\`);`);
-			code.push('  logger.error(err);');
-			code.push('  return { statusCode: 500, body: err, headers: options.headers };');
-			code.push('}');
+		code.push(`${tab(1)}logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Starting ${_.camelCase(stage._id)} Stage\`);`);
+		code.push(`${tab(1)}try {`);
+		if ((stage.type === 'API' || stage.type === 'FAAS' || stage.type === 'DATA_SERVICE') && stage.outgoing) {
+			code.push(`${tab(2)}const options = {};`);
+			code.push(`${tab(2)}state.url = '${stage.outgoing.url}';`);
+			code.push(`${tab(2)}state.method = '${stage.outgoing.method}';`);
+			code.push(`${tab(2)}options.url = '${stage.outgoing.url}';`);
+			code.push(`${tab(2)}options.method = '${stage.outgoing.method}';`);
+			code.push(`${tab(2)}options.headers = _.merge(state.headers, ${JSON.stringify(stage.outgoing.headers)});`);
+			code.push(`${tab(2)}options.json = state.body;`);
+			code.push(`${tab(2)}try {`);
+			code.push(`${tab(3)}const tempResponse = await httpClient.request(options);`);
+			code.push(`${tab(3)}if (tempResponse && tempResponse.statusCode != 200) {`);
+			code.push(`${tab(4)}logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with not 200\`);`);
+			code.push(`${tab(4)}return { statusCode: tempResponse.statusCode, body: tempResponse.body, headers: tempResponse.headers };`);
+			code.push(`${tab(3)}}`);
+			code.push(`${tab(3)}logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with 200\`);`);
+			code.push(`${tab(3)}return { statusCode: tempResponse.statusCode, body: tempResponse.body, headers: tempResponse.headers };`);
+			code.push(`${tab(2)}} catch (err) {`);
+			code.push(`${tab(3)}logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with Error\`);`);
+			code.push(`${tab(3)}logger.error(err);`);
+			code.push(`${tab(3)}return { statusCode: 500, body: err, headers: options.headers };`);
+			code.push(`${tab(2)}}`);
 		} else if (stage.type === 'TRANSFORM' && stage.mapping) {
-			code.push('const newBody = {};');
-			code.push('if (Array.isArray(state.body)) {');
-			code.push('  state.body.forEach( item => {');
-			stage.mapping.forEach(item => {
-				code.push(`    _.set(newBody, '${item.target}', _.get(item, '${item.source[0]}'));`);
+			code.push(`${tab(2)}const newBody = {};`);
+			code.push(`${tab(2)}if (Array.isArray(state.body)) {`);
+			code.push(`${tab(3)}state.body.forEach(stage => {`);
+			stage.mapping.forEach(stage => {
+				code.push(`${tab(4)}_.set(newBody, '${stage.target}', _.get(stage, '${stage.source[0]}'));`);
 			});
-			code.push('  });');
-			code.push('} else {');
-			stage.mapping.forEach(item => {
-				code.push(`_.set(newBody, '${item.target}', _.get(state.body, '${item.source[0]}'));`);
+			code.push(`${tab(3)}});`);
+			code.push(`${tab(2)}} else {`);
+			stage.mapping.forEach(stage => {
+				code.push(`${tab(3)}_.set(newBody, '${stage.target}', _.get(state.body, '${stage.source[0]}'));`);
 			});
-			code.push('}');
-			code.push('  return { statusCode: 200, body: newBody, headers: state.headers };');
+			code.push(`${tab(2)}}`);
+			code.push(`${tab(2)}return { statusCode: 200, body: newBody, headers: state.headers };`);
+		} else if (stage.type === 'FLOW') {
+			if (stage.parallel && stage.parallel.length > 0) {
+				code.push(`${tab(2)}let promiseArray = [];`);
+				stage.parallel.forEach(flow => {
+					code.push(`${tab(2)}promiseArray.push(callFlow('${flow._id}', state))`);
+				});
+				code.push(`${tab(2)}const promises = await Promise.all(promiseArray)`);
+				code.push(`${tab(2)}const allBody = promises.map(e=>e.body)`);
+				code.push(`${tab(2)}const allHeaders = promises.reduce((prev,curr)=>_.merge(prev,curr.headers),{})`);
+				code.push(`${tab(2)}return { statusCode: 200, body: allBody, headers: allHeaders };`);
+			} else if (stage.sequence && stage.sequence.length > 0) {
+				code.push(`${tab(2)}let tempResponse = state;`);
+				stage.sequence.forEach(flow => {
+					code.push(`${tab(2)}tempResponse = await callFlow('${flow._id}', tempResponse)`);
+					code.push(`${tab(2)}if( tempResponse && tempResponse.statusCode != 200 ) {`);
+					code.push(`${tab(3)}logger.info(\`[\${req.header('data-stack-txn-id')}] [\${req.header('data-stack-remote-txn-id')}] Ending ${_.camelCase(stage._id)} Stage with not 200\`);`);
+					code.push(`${tab(3)}return { statusCode: tempResponse.statusCode, body: tempResponse.body, headers: tempResponse.headers };`);
+					code.push(`${tab(2)}}`);
+				});
+			}
+		} else if (stage.type === 'FOREACH' || stage.type === 'REDUCE') {
+			code = _.concat(code, generateStages(stage));
 		} else {
-			code.push('  return { statusCode: 200, body: state.body, headers: state.headers };');
+			code.push(`${tab(2)}return { statusCode: 200, body: state.body, headers: state.headers };`);
 		}
-		code.push('    } catch (err) {');
-		code.push('        logger.error(err);');
-		code.push('        return { statusCode: 500, body: err, headers: state.headers };');
-		code.push('    }');
-		code.push('}');
+		code.push(`${tab(1)}} catch (err) {`);
+		code.push(`${tab(2)}logger.error(err);`);
+		code.push(`${tab(2)}return { statusCode: 500, body: err, headers: state.headers };`);
+		code.push(`${tab(1)}}`);
+		code.push(`}`);
 	});
 	return _.concat(code, exportsCode).join('\n');
 }
 
 
-module.exports.generateCode = generateCode;
-module.exports.generateStages = generateStages;
+
+
+module.exports.parseFlow = parseFlow;
+module.exports.parseStages = parseStages;
--- generator/test.js
+++ generator/test.js
@@ -3,8 +3,8 @@ const { writeFileSync } = require('fs');
 const codeGen = require('./code.generator');
 const sampleJSON = require('./sample.json');
 
-const stages = codeGen.generateStages(sampleJSON);
-const code = codeGen.generateCode(sampleJSON);
+const stages = codeGen.parseStages(sampleJSON);
+const code = codeGen.parseFlow(sampleJSON);
 
 writeFileSync('../stage.utils.js', stages);
 writeFileSync('../route.js', code);
