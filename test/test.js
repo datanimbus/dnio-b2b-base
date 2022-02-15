bbdf9d06b6a2ad213c36933d69f302cde6a67b88
Jerry M <developer.jerrymannel@gmail.com>
2022-02-16 00:09:25 +0530

    updated schema

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ const { writeFileSync } = require('fs');
 const codeGen = require('../generator/code.generator');
 const validator = require('../generator/schema.validator');
 
-const sampleJSON = require('./test.01.simpleAPI.json');
+const sampleJSON = require('./test.01.API.JSON.json');
 // const sampleJSON = require('./test.01.error.json');
 
 try{
 	validator(sampleJSON);
-	const stages = codeGen.generateStages(sampleJSON);
-	const code = codeGen.generateCode(sampleJSON);
+	const stages = codeGen.parseStages(sampleJSON);
+	const code = codeGen.parseFlow(sampleJSON);
 	console.log("Flow code generated!");
 	writeFileSync('../stage.utils.js', stages);
 	writeFileSync('../route.js', code);
