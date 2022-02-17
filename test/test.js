0aac8c4235b51f2c83057cf2d12bdd340f3f09ec
Jerry M <developer.jerrymannel@gmail.com>
2022-02-17 13:26:28 +0530

    Resolved merge conflicts

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-const { writeFileSync } = require('fs');
+// const { writeFileSync } = require('fs');
 
-const codeGen = require('../generator/code.generator');
+// const codeGen = require('../generator/code.generator');
 const validator = require('../generator/schema.validator');
 
 // const sampleJSON = require('./test.01.API.JSON.json');
@@ -9,13 +9,13 @@ const validator = require('../generator/schema.validator');
 // const sampleJSON = require('./test.04.Branch.WithCondition.json');
 const sampleJSON = require('./test.05.Dataservice.POST.json');
 
-try{
+try {
 	validator(sampleJSON);
-	const stages = codeGen.parseStages(sampleJSON);
-	const code = codeGen.parseFlow(sampleJSON);
-	console.log("Flow code generated!");
-	writeFileSync('../stage.utils.js', stages);
-	writeFileSync('../route.js', code);
-} catch(e){
+	// const stages = codeGen.parseStages(sampleJSON);
+	// const code = codeGen.parseFlow(sampleJSON);
+	// console.log("Flow code generated!");
+	// writeFileSync('../stage.utils.js', stages);
+	// writeFileSync('../route.js', code);
+} catch (e) {
 	console.log(e.message);
 }
