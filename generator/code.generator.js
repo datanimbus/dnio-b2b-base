f40a1a36a185d36a3113786054eafe6003be9516
Jugnu Agrawal <jugnu@appveen.com>
2022-09-28 16:56:19 +0530

    logging path

--- generator/code.generator.js
+++ generator/code.generator.js
@@ -45,7 +45,8 @@ function parseFlow(dataJson) {
 	code.push('const xmlParser = new XMLParser();');
 	code.push('');
 	// TODO: Method to be fixed.
-	code.push(`router.${(inputNode.options.method || 'POST').toLowerCase()}('${api}', async function (req, res) {`);
+	// code.push(`router.${(inputNode.options.method || 'POST').toLowerCase()}('${api}', async function (req, res) {`);
+	code.push(`router.post('${api}', async function (req, res) {`);
 	code.push(`${tab(1)}let txnId = req.headers['data-stack-txn-id'];`);
 	code.push(`${tab(1)}let remoteTxnId = req.headers['data-stack-remote-txn-id'];`);
 	code.push(`${tab(1)}let response = req;`);
--- lib.middlewares.js
+++ lib.middlewares.js
@@ -6,11 +6,12 @@ const logger = log4js.getLogger(global.loggerName);
 let e = {};
 
 e.addHeaders = (_req, _res, _next) => {
-	if(!_req.headers['data-stack-txn-id']) {
+	logger.info(`Getting Request: ${_req.path}`);
+	if (!_req.headers['data-stack-txn-id']) {
 		_req.headers['data-stack-txn-id'] = uuid();
 		logger.info(`No txn id found. Setting txn id to : ${_req.headers['data-stack-txn-id']}`);
 	}
-	if(!_req.headers['data-stack-remote-txn-id']) {
+	if (!_req.headers['data-stack-remote-txn-id']) {
 		_req.headers['data-stack-remote-txn-id'] = `${uuid()}`;
 		logger.info(`No remote txn id found. Setting remote txn id to : ${_req.headers['data-stack-remote-txn-id']}`);
 	}
