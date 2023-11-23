94dcade9414b9869fbe24150872f9c4f08db2eab
Jugnu Agrawal <jugnu@appveen.com>
2023-11-23 17:20:10 +0530

    duplicate name fix

--- generator/code.generator.js
+++ generator/code.generator.js
@@ -2438,8 +2438,9 @@ function generateFileConvertorCode(node, code, dataStructureType) {
 		}
 	}
 	code.push(`${tab(2)}let ext_1 = '${ext}';`);
-	code.push(`${tab(2)}let outputFileName = '${node._id}' + ext_1;`);
-	code.push(`${tab(2)}const filePath = path.join(process.cwd(), 'downloads', outputFileName);`);
+	code.push(`${tab(2)}let uniqueId = commonUtils.getUniqueID();`);
+	code.push(`${tab(2)}let outputFileName_1 = '${node._id}_'+ uniqueId + ext_1;`);
+	code.push(`${tab(2)}const filePath = path.join(process.cwd(), 'downloads', outputFileName_1);`);
 
 	code.push(`${tab(3)}const renderOptions = {};`);
 	code.push(`${tab(3)}renderOptions.filePath = filePath;`);
